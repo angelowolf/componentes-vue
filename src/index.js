175ef7d8218f135b0863d2f808f96fe7e0550f28
angelo wolf <angelowolf21@gmail.com>
2017-06-24 21:19:42 -0300

    release 1.0.0

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import Hello from './Hello.vue'
-import HelloJsx from './Hello.jsx'
+import Datepicker from './Datepicker'
+import Boton from './botones/Boton'
+import BotonGuardar from './botones/BotonGuardar'
+import BotonEditar from './botones/BotonEditar'
+import BotonEliminar from './botones/BotonEliminar'
+import BotonBuscar from './botones/BotonBuscar'
+import FormHelp from './FormHelp'
 
 function plugin (Vue) {
-  Vue.component('hello', Hello)
-  Vue.component('hello-jsx', HelloJsx)
+  Vue.component('boton', Boton)
+  Vue.component('boton-guardar', BotonGuardar)
+  Vue.component('boton-buscar', BotonBuscar)
+  Vue.component('boton-editar', BotonEditar)
+  Vue.component('boton-eliminar', BotonEliminar)
+  Vue.component('form-help', FormHelp)
+  Vue.component('fecha', Datepicker)
 }
 
 // Install by default if using the script tag
@@ -15,7 +25,12 @@ export default plugin
 const version = '__VERSION__'
 // Export all components too
 export {
-  Hello,
-  HelloJsx,
+  Datepicker,
+  Boton,
+  BotonGuardar,
+  BotonEditar,
+  BotonEliminar,
+  BotonBuscar,
+  FormHelp,
   version
 }
